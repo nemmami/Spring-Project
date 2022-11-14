96c0e84d9ce0021a322e37356cdc8b1a7fe80a42
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-11-14 14:13:14 +0100

    add methode readOne in PassengersService

--- passengers/src/main/java/be/vinci/ipl/projet/passengers/PassengersService.java
+++ passengers/src/main/java/be/vinci/ipl/projet/passengers/PassengersService.java
@@ -6,4 +6,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class PassengersService {
 
+  private final PassengersRepository repository;
+
+  public PassengersService(PassengersRepository repository) {
+    this.repository = repository;
+  }
+
+  /**
+   * Reads a passenger in repository
+   * @param id ID of the passenger
+   * @return The passenger, or null if it couldn't be found
+   */
+  public Passenger readOne(long id) {
+    return repository.findById(id).orElse(null);
+  }
+
+
+
 }
