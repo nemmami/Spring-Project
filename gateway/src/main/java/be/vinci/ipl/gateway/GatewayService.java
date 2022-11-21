e8b538b36deb6491070f39344d5ad07ca0e5e342
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2022-11-21 15:46:50 +0100

    first part of GatewayService

--- gateway/src/main/java/be/vinci/ipl/gateway/GatewayService.java
+++ gateway/src/main/java/be/vinci/ipl/gateway/GatewayService.java
@@ -4,6 +4,8 @@ package be.vinci.ipl.gateway;
 import be.vinci.ipl.gateway.data.AuthenticationProxy;
 import be.vinci.ipl.gateway.data.TripsProxy;
 import be.vinci.ipl.gateway.data.UsersProxy;
+import be.vinci.ipl.gateway.models.Credentials;
+import be.vinci.ipl.gateway.models.NewUser;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,4 +21,28 @@ public class GatewayService {
     this.tripsProxy = tripsProxy;
     this.usersProxy = usersProxy;
   }
+
+  public String connect(Credentials credentials){
+    return authenticationProxy.connect(credentials);
+  }
+
+  public String verify(String token){
+    return authenticationProxy.verify(token);
+  }
+
+  public void createUser(NewUser newUser){
+    authenticationProxy.createCredentials(newUser.getEmail(), new Credentials(newUser.getEmail(),
+        newUser.getPassword()));
+    usersProxy.createUser( newUser);
+  }
+
+  public void findUserByMail(String mail){
+    usersProxy.findUserEmail(mail);
+  }
+
+  public void UpdatePassword(Credentials credentials){
+    usersProxy.updatePassword(credentials);
+    authenticationProxy.updateCredentials(credentials.getEmail(), credentials);
+  }
+
 }
