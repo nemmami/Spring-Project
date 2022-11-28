942c2d6dc6de12d898f3f7e2d3492706b5d9a5f1
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2022-11-28 14:21:53 +0100

    changes return values in TripsProxy

--- gateway/src/main/java/be/vinci/ipl/gateway/data/TripsProxy.java
+++ gateway/src/main/java/be/vinci/ipl/gateway/data/TripsProxy.java
@@ -1,6 +1,7 @@
 package be.vinci.ipl.gateway.data;
 
 import be.vinci.ipl.gateway.models.*;
+import java.util.ArrayList;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,25 +22,25 @@ public interface TripsProxy {
 
 //pas sur de la façon dont j'ai fait tt ça tout param sont optionels ici
 @GetMapping("/trips")
-  void getListTrips(@RequestParam("departure_date") String departureDate,
+ArrayList<Trip> getListTrips(@RequestParam("departure_date") String departureDate,
     @RequestParam("originLon") String originLon, @RequestParam("destinationLat") String destinationLat,
     @RequestParam("destinationLon") String destinationLon);
 
 
 @GetMapping("/trips/{id}")
-  void getTripInfo(@PathVariable int id);
+  Trip getTripInfo(@PathVariable int id);
 
 @DeleteMapping("trips/{id}")
   void deleteTrip(@PathVariable int id);
 
 @GetMapping("/trips/{id}/passengers")
-  void getPassengerList(@PathVariable int id);
+  ArrayList<Passengers> getPassengerList(@PathVariable int id);
 
 @PostMapping("/trips/{trip_id}/passengers/{user_id}")
   void addPasengerToTrip(@PathVariable int trip_id, @PathVariable int user_id);
 
 @GetMapping("/trips/{trip_id}/passengers/{user_id}")
-  void getPassengerStatus(@PathVariable int trip_id, @PathVariable int user_id);
+  String getPassengerStatus(@PathVariable int trip_id, @PathVariable int user_id);
 
 @PutMapping("/trips/{trip_id}/passengers/{user_id}")
   void updatePassengerStatus(@PathVariable int trip_id, @PathVariable int user_id, @RequestParam("status") String status);
