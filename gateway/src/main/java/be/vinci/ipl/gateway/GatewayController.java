cb54b45f6afa4fcac6a9962a7d5c0f70d566cc66
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2022-11-28 15:08:53 +0100

    push to pull master

--- gateway/src/main/java/be/vinci/ipl/gateway/GatewayController.java
+++ gateway/src/main/java/be/vinci/ipl/gateway/GatewayController.java
@@ -1,6 +1,8 @@
 package be.vinci.ipl.gateway;
 
 
+import be.vinci.ipl.gateway.models.Credentials;
+import be.vinci.ipl.gateway.models.NewUser;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,5 +17,16 @@ public class GatewayController {
 
   public GatewayController(GatewayService service) {
     this.service = service;
+  }
+
+  @PostMapping("/auth")
+  String  connect(@RequestBody Credentials credentials){
+    return service.connect(credentials);
+  }
+
+  @PostMapping("/user")
+  ResponseEntity<Void> createUser(@RequestBody NewUser newUser){
+    service.createUser(newUser);
+    return new ResponseEntity<>(HttpStatus.CREATED);
   }
 }
