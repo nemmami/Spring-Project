5655801b5ebddc7b0d51ed58fdfbe71f35a2cb32
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2022-11-28 14:48:49 +0100

    push GatewayController

--- gateway/src/main/java/be/vinci/ipl/gateway/GatewayController.java
+++ gateway/src/main/java/be/vinci/ipl/gateway/GatewayController.java
@@ -2,13 +2,18 @@ package be.vinci.ipl.gateway;
 
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin(origins = "http://localhost")
 @RestController
 public class GatewayController {
 
-  public final GatewayService gatewayService;
+  public final GatewayService service;
 
-  public GatewayController(GatewayService gatewayService) {
-    this.gatewayService = gatewayService;
+  public GatewayController(GatewayService service) {
+    this.service = service;
   }
 }
