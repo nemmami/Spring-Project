57e9342aec8af9e1bc46a99dff31b6b72e19d2f3
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-01 22:44:58 +0100

    add file PassengerUsers

--- mock_passengers/src/main/java/be/vinci/ipl/projet/mock_passengers/Passenger.java
+++ mock_passengers/src/main/java/be/vinci/ipl/projet/mock_passengers/Passenger.java
@@ -14,4 +14,8 @@ public class Passenger {
   private int tripId;
   private int userId;
   private PassengerStatus status;
+
+  public static Passenger defPassenger(int tripId, int userId) {
+    return new Passenger(0, tripId, userId, PassengerStatus.PENDING);
+  }
 }
---
+++ mock_passengers/src/main/java/be/vinci/ipl/projet/mock_passengers/PassengerUsers.java
@@ -0,0 +1,7 @@
+package be.vinci.ipl.projet.mock_passengers;
+
+public class PassengerUsers {
+  private ArrayList<User> pending;
+  private ArrayList<User> accepted;
+  private ArrayList<User> refused;
+}
