f6c2fe58fb6b8a0f247a04537508b219e47bd88b
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-01 12:45:29 +0100

    add class Trip

---
+++ passengers/src/main/java/be/vinci/ipl/projet/passengers/models/Trip.java
@@ -0,0 +1,18 @@
+package be.vinci.ipl.projet.passengers.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Trip {
+  private int id;
+  private Position origin;
+  private Position destination;
+  private String departure;
+  private int dirverId;
+  private int availableSeating;
+
+}
+
