2baa61c3352ed3124104341ed5666fa217e95ece
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-01 22:46:51 +0100

    add file PassengerTrips

---
+++ mock_passengers/src/main/java/be/vinci/ipl/projet/mock_passengers/PassengerTrips.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.projet.mock_passengers;
+
+public class PassengerTrips {
+
+  private ArrayList<Trip> pending;
+  private ArrayList<Trip> accepted;
+  private ArrayList<Trip> refused;
+}
