3a808da0adf007ece654b1c0c7ccbf526fea8a75
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-01 22:24:51 +0100

    add new service mock_passengers

---
+++ mock_passengers/src/main/java/be/vinci/ipl/projet/mock_passengers/MockPassengersApplication.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.projet.mock_passengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPassengersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MockPassengersApplication.class, args);
+  }
+
+}
